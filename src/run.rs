325bd1394f0a740b7df89c91eb35c7da2b598d2e
jac3km4 <jac3km4@gmail.com>
2020-11-16 18:42:04 +0000

    Allow use of any thread

--- src/run.rs
+++ src/run.rs
@@ -7,6 +7,10 @@ use std::future::Future;
 use std::sync::Arc;
 use winit::event::Event as WinitEvent;
 use winit::event_loop::{ControlFlow, EventLoop};
+#[cfg(target_os = "unix")]
+use winit::platform::unix::EventLoopExtUnix;
+#[cfg(target_os = "windows")]
+use winit::platform::windows::EventLoopExtWindows;
 
 /// The entry point for a blinds-based application
 ///
@@ -24,7 +28,7 @@ where
     let stream = EventStream::new();
     let buffer = stream.buffer();
 
-    let event_loop = EventLoop::new();
+    let event_loop = EventLoop::new_any_thread();
     let window = Arc::new(WindowContents::new(&event_loop, settings));
     let pool = LocalPool::new();
     pool.spawner()
